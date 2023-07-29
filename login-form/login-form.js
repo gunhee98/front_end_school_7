27399136d7eece26b7eaca2950d646c28aebea32
gunhee98 <rjsgmlaha1@gmail.com>
2023-07-29 21:36:26 +0900

    update login-form

--- login-form/login-form.js
+++ login-form/login-form.js
@@ -1,13 +1,27 @@
 const loginForm = document.querySelector(".login-form");
 const textId = document.querySelector(".text-id");
+const textPw = document.querySelector(".text-pw");
 const inputId = document.querySelector("#id");
+const inputPw = document.querySelector("#pw");
 loginForm.addEventListener("submit", function (e) {
   e.preventDefault();
   if (inputId.value === "") {
     console.log("hello");
     textId.classList.add("show");
   }
+  if (inputPw.value === "") {
+    console.log("hello");
+    textPw.classList.add("show");
+  }
 });
-if (inputId.value !== "") {
-  textId.classList.remove("show");
-}
+
+inputId.addEventListener("input", () => {
+  if (inputId.value !== "") {
+    textId.classList.remove("show");
+  }
+});
+inputPw.addEventListener("input", () => {
+  if (inputPw.value !== "") {
+    textPw.classList.remove("show");
+  }
+});
